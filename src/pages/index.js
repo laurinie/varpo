a9434d10b4510c255e0667e12107594f5b12e96b
laurinie <niemi.late@gmail.com>
2019-06-13 10:08:30 +0300

    Add frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,25 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import  ReactMarkdown from  'react-markdown';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-const Johtaja = ({node})=>{
-  return (
-    <li>
-      <Link to={node.name}>{node.name}</Link>
-    </li>
-  )
-}
+// }
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <ul>{data.allContentfulJohtaja.edges.map((edge)=><Johtaja node={edge.node}/>)}</ul>
+    <div>< ReactMarkdown source={data.allContentfulEtusivuFrontpageContentTextNode.edges[0].node.frontpageContent}/></div>
   </Layout>
 )
 
 export default IndexPage
 
 export const pageQuery = graphql`
-query pageQuery{
-  allContentfulJohtaja(filter:{
-    node_locale:{eq:"fi-FI"}
-  }){
-    edges{
-      node{
-        name
+query MyQuery {
+  allContentfulEtusivuFrontpageContentTextNode {
+    edges {
+      node {
+        frontpageContent
       }
     }
   }
---
+++ src/pages/johtajat.js
@@ -0,0 +1,34 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+const Johtaja = ({node})=>{
+  return (
+    <li>
+      <Link to={node.name}>{node.name}</Link>
+    </li>
+  )
+}
+const Johtajat = ({data}) => (
+  <Layout>
+    <SEO title="Home" />
+    <ul>{data.allContentfulJohtaja.edges.map((edge)=><Johtaja node={edge.node}/>)}</ul>
+  </Layout>
+)
+
+export default Johtajat
+
+export const pageQuery = graphql`
+query pageQuery{
+  allContentfulJohtaja(filter:{
+    node_locale:{eq:"fi-FI"}
+  }){
+    edges{
+      node{
+        name
+      }
+    }
+  }
+}`
