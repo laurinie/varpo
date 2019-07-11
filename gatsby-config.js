d7cd3e36f3db4dad9b4d714c63e868640467dbd3
laurinie <niemi.late@gmail.com>
2019-07-11 13:47:43 +0300

    Fix meta colors and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Varpo`,
-    description: `Vartiovuoren Poikien nettisivut`,
+    description: `Vartiovuoren Pojat - Erähenkinen partiolippukunta Itä-Helsingistä. Vartiovuoren Pojat eli lyhennettynä Varpo on pääkaupunkiseudun suurin poikalippukunta.`,
     author: `@laurinie`,
     menuLinks: [
       {
@@ -59,13 +59,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Varpo`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#6e0000`,
+        theme_color: `#6e0000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/varpo_tunnus-color-transparent.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
