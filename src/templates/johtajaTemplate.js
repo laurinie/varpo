2df78bf6b0dcad6b73563ef6a15ecc340acd45e2
laurinie <niemi.late@gmail.com>
2019-08-20 20:27:24 +0300

    Remove johtajat loggign

--- src/templates/johtajaTemplate.js
+++ src/templates/johtajaTemplate.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 
 class Johtaja extends Component {
     render() {
-        console.log(this.props.data.contentfulJohtaja)
+        // console.log(this.props.data.contentfulJohtaja)
         const j = this.props.data.contentfulJohtaja
 
         return (
