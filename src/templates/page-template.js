c51578f7d4fc0edfa94480f12771215280ed61be
laurinie <niemi.late@gmail.com>
2020-04-07 20:50:42 +0300

    Fix links

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -42,7 +42,7 @@ class PageTemplate extends React.Component {
             {post.coverImage&&<NonStretchedImage className={pageStyles.coverImage} objectFit={"cover"} fluid={post.coverImage.fluid} />}
           </div>
           <div className="wrapper">
-            <Link to={"/"}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" /><path d="M0 0h24v24H0V0z" fill="none" /></svg>Etusivulle</Link>
+            <Link to={"/"} className={pageStyles.back}><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" /><path d="M0 0h24v24H0V0z" fill="none" /></svg>Etusivulle</Link>
             {post.blocks.map(block => (
               this.renderBlock(block)
             ))}
